06d4c42acbd4cb9b1e8e49a960555252bbccc16b
lucia027 <fuertes.lucia.27@gmail.com>
2026-04-14 10:55:40 +0200

    Modelos creados - Sin testear

---
+++ Itv/Itv/Models/Vehiculo.cs
@@ -0,0 +1,17 @@
+﻿using Itv.Enums;
+
+namespace Itv.Models;
+
+/// <summary>
+/// Representa a un vehiculo en el sistema.
+/// </summary>
+public record Vehiculo {
+    public int Id { get; init; }
+    public string Matricula { get; set; } = string.Empty;
+    public string Marca { get; set; } = string.Empty;
+    public string Modelo { get; set; } = string.Empty;
+    public int Cilindrada { get; set; }
+    public Motor Motor { get; set; }
+    public string DniDueño { get; set; } = string.Empty;
+    public bool IsDelete { get; set; }
+}
