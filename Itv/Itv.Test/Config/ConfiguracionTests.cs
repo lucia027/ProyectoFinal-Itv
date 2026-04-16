b80a89b2f703976ea0bec1266c86a16d35bcafac
lucia027 <fuertes.lucia.27@gmail.com>
2026-04-16 13:13:35 +0200

    Terminada la clas de la configuracion y appsetings, inicio de test con errores y sin terminar

---
+++ Itv/Itv.Test/Config/ConfiguracionTests.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using Itv.Config;
+
+namespace Itv.Test.Config;
+
+[TestFixture]
+public class ConfiguracionTests {
+
+    [TestFixture]
+    public sealed class Propiedades() {
+
+        [Test]
+        public void Locale_RetornaEspaña() {
+            //Act
+            var locale = Configuracion.Locale;
+            
+            //Assert
+            locale.Should().NotBeNull();
+            locale.Name.Should().Be("es-ES");
+        }
+    }
+}
