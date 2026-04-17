d6929cf0df31bd5be22f61cc560fb2bb05eebcd8
lucia027 <lucia.fuertes.06@gmail.com>
2026-04-17 17:04:18 +0200

    Avance en el storage json.

---
+++ Itv/Itv.Test/Storage/Json/VehiculoJsonStorageTests.cs
@@ -0,0 +1,30 @@
+﻿using Itv.Storage.Json;
+
+namespace Itv.Test.Storage.Json;
+
+[TestFixture]
+public class VehiculoJsonStorageTests {
+
+    [TestFixture]
+    public sealed class CasosPositivos() {
+        [SetUp]
+        public void SetUp() {
+            _storage = new VehiculoJsonStorage();
+            _tempPath = Path.Combine(Path.GetTempPath(), $"{Guid.NewGuid()}.json");
+        }
+
+        [TearDown]
+        public void TearDown() {
+            if(File.Exists(_tempPath)) File.Delete(_tempPath);
+        }
+        
+        private VehiculoJsonStorage _storage;
+        private string _tempPath;
+
+        [Test]
+        public void Cargar_DatosValidos_CargarDatos() {
+        }
+
+    }
+
+}
---
+++ Itv/Itv/Storage/Common/IStorage.cs
@@ -0,0 +1,20 @@
+﻿using CSharpFunctionalExtensions;
+using Itv.Errors.Common;
+
+namespace Itv.Storage.Common;
+
+public interface IStorage<T> {
+    /// <summary>
+    /// Carga los datos del archivo con la ruta especificada.
+    /// </summary>
+    /// <param name="path">Ruta del archivo de datos.</param>
+    /// <returns>Enumerable de los datos cargados.</returns>
+    Result<IEnumerable<T>, DomainError> Cargar(string path);
+    
+    /// <summary>
+    /// Salva los datos en un archivo con la ruta especificada.
+    /// </summary>
+    /// <param name="items">Datos a salvar.</param>
+    /// <param name="path">Ruta donde estara el archivo.</param>
+    Result<bool, DomainError> Salvar(IEnumerable<T> items, string path);
+}
