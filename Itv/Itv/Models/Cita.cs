b3303de6e7ad401715e001003ad6b7ad864decb1
lucia027 <fuertes.lucia.27@gmail.com>
2026-04-30 12:36:47 +0200

    Añadido de loggers en el validador.

--- Itv/Itv/Models/Cita.cs
+++ Itv/Itv/Models/Cita.cs
@@ -9,7 +9,7 @@ public record Cita {
     public int Id { get; init; }
     public string Matricula { get; set; } = string.Empty;
     public string Marca { get; set; } = string.Empty;
-    public string Modelo { get; set; } = string.Empty;
+    public string Modelo { get; set; } = string.Empty; 
     public int Cilindrada { get; set; }
     public Motor Motor { get; set; }
     public string DniDueño { get; set; } = string.Empty;
@@ -18,4 +18,9 @@ public record Cita {
     public DateTime CreateAt { get; set; }
     public DateTime? UpdateAt { get; set; }
     public bool IsDelete { get; set; }
+
+    public override string ToString() {
+        var updateAt = UpdateAt.ToString() ?? "No se ha actualizado";
+        return $"Cita: [Id-{Id}, Matricula-{Matricula}, Marca-{Marca}, Modelo-{Modelo}, Cilindrada-{Cilindrada}, Motor-{Motor.ToString()}, Dni dueño-{DniDueño}, Fecha matriculacion-{FechaMatriculacion}, Fecha inspeccion-{FechaInspeccion}, Hora de creacion-{CreateAt}, Hora de modificacion-{updateAt}, Ha sido eliminado-{IsDelete}]";
+    }
 }
--- Itv/Itv/Validator/CitaValidator.cs
+++ Itv/Itv/Validator/CitaValidator.cs
@@ -5,15 +5,16 @@ using Itv.Errors;
 using Itv.Errors.Common;
 using Itv.Models;
 using Itv.Validator.Common;
+using Serilog;
 
 namespace Itv.Validator;
 
-public class 
-    
-    CitaValidator : IValidator<Cita> {
+public class CitaValidator : IValidator<Cita> {
+    private readonly ILogger _logger = Log.ForContext<CitaValidator>();
     
     /// <inheritdoc cref="IValidator.Validate" />
     public Result<Cita, DomainError> Validate(Cita entity) {
+        _logger.Debug($"Intentando validar la entidad: {entity}");
         
         var regexMatricula = @"^[0-9]{4}[BCDFGHJKLMNPRSTVWXYZ]{3}$";
         var regexDni = @"^[0-9]{8}[TRWAGMYFPDXBNJZSQVHLCKE]$";
@@ -45,7 +46,10 @@ public class
             errores.Add("La fecha de inspeccion no puede ser superior a 30 dias.");
         }
 
-        if (errores.Any()) return Result.Failure<Cita, DomainError>(CitaErrors.Validation(errores));
+        if (errores.Any()) {
+            _logger.Warning($"No se ha podido validar la entidad: {entity}");
+            return Result.Failure<Cita, DomainError>(CitaErrors.Validation(errores));
+        }
         return Result.Success<Cita, DomainError>(entity);
     }
 
@@ -55,6 +59,7 @@ public class
     /// <param name="dni">DNi proporcionado.</param>
     /// <returns>Verdadero en caso correcto y false en contrario.</returns>
     private bool ComprobarDniValido(string dni) {
+        _logger.Debug($"Intentando comprobar el dni: {dni}.");
         char[] letrasPermitidas = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'];
 
         try {
@@ -69,6 +74,7 @@ public class
 
             return letraProporcionada == letraCorrecta;
         } catch (Exception) {
+            _logger.Error($"El dni: {dni} no ha pasado el proceso de validacion.");
             return false;
         }
     }
