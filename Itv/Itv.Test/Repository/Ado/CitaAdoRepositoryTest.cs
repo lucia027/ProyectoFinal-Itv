c475b524eba4baa4095997923fc24593d8bc2d26
lucia027 <lucia.fuertes.06@gmail.com>
2026-05-07 18:49:46 +0200

    Empiezo test

---
+++ Itv/Itv.Test/Repository/Ado/CitaAdoRepositoryTest.cs
@@ -0,0 +1,16 @@
+﻿namespace Itv.Test.Repository.Ado;
+
+[TestFixture]
+public class CitaAdoRepositoryTest {
+
+    [TestFixture]
+    public sealed class CasosValidos {
+        
+        public void 
+        
+    }
+
+    [TestFixture]
+    public sealed class CasosInvalidos {
+    }
+}
